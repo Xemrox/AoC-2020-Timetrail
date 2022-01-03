3bc408e90abada753490a60b4547079334f13ec2
Xemrox <xemrox@users.noreply.github.com>
2022-01-03 07:32:38 +0100

    Generic PuzzleLoader for input files #1

---
+++ Xemrox.Shared/Puzzle.cs
@@ -0,0 +1,13 @@
+﻿namespace Xemrox.Shared;
+
+public readonly struct Puzzle<T>
+{
+    public readonly string Name { get; init; }
+    public readonly T[] Value { get; init; }
+
+    public Puzzle(string name, T[] value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
---
+++ Xemrox.Shared/PuzzleLoader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Xemrox.Shared;
+public static class PuzzleLoader
+{
+    /// <summary>
+    /// Loads a well defined puzzle input file <paramref name="fileName"/> and transforms it according to <paramref name="contentTransformation"/>
+    /// </summary>
+    /// <typeparam name="T">Type of puzzle input</typeparam>
+    /// <param name="fileName">Target filename</param>
+    /// <param name="contentTransformation">Content transformation function</param>
+    /// <returns>Instance of <see cref="Puzzle{T}" /></returns>
+    /// <exception cref="FileNotFoundException"></exception>
+    public static Puzzle<T> LoadInputFile<T>(string fileName, Func<string, T> contentTransformation)
+    {
+        if (!File.Exists(fileName))
+            throw new FileNotFoundException(fileName);
+
+        var shortFileName = Path.GetFileNameWithoutExtension(fileName);
+        var puzzleName = shortFileName
+            .Split('_')
+            .First();
+
+        var fileLines = File.ReadAllLines(fileName, System.Text.Encoding.UTF8);
+        var transformedContent = fileLines
+            .Select(contentTransformation)
+            .ToArray();
+
+        return new Puzzle<T>(
+            puzzleName,
+            transformedContent
+        );
+    }
+
+    /// <summary>
+    /// Loads all lines of a puzzle file as raw strings
+    /// </summary>
+    /// <param name="fileName">The file to load</param>
+    /// <returns>Instance of <see cref="Puzzle{T}" /> whose generic type argument is <see cref="string"/> </returns>
+    public static Puzzle<string> LoadInputFile(string fileName)
+    {
+        return LoadInputFile(fileName, line => line);
+    }
+}
